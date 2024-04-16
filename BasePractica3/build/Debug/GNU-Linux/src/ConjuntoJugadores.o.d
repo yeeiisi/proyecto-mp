9709852fd4088165d47760c75a86c6bc3506ae9a
yeeiisi <127243820+yeeiisi@users.noreply.github.com>
2024-04-16 16:48:29 +0200

    Add files via upload

---
+++ BasePractica3/build/Debug/GNU-Linux/src/ConjuntoJugadores.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux/src/ConjuntoJugadores.o: src/ConjuntoJugadores.cpp \
+ include/ConjuntoJugadores.h include/Jugador.h
+include/ConjuntoJugadores.h:
+include/Jugador.h:
---
+++ BasePractica3/build/Debug/GNU-Linux/src/Jugador.o.d
@@ -0,0 +1,2 @@
+build/Debug/GNU-Linux/src/Jugador.o: src/Jugador.cpp include/Jugador.h
+include/Jugador.h:
